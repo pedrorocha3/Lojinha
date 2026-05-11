b6344b8a93d52ed96ced3623e61089d06b08e4ef
Jose <roniele.bio@gmail.com>
2026-05-11 17:49:21 -0400

    string conecção

---
+++ Lojinha/DAL/Dados.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lojinha.DAL
+{
+    public class Dados
+    {
+        public static string StringConexao
+        {
+            get
+            {
+                return "server = Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = Lojinha; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False";
+            }
+
+
+
+
+        }
+    } 
+}
