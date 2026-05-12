6709006915726478f2050ff718333433e42dca26
Jose <roniele.bio@gmail.com>
2026-05-12 14:34:57 -0400

    jhon

--- Lojinha/DAL/ClientesDAL.cs
+++ Lojinha/DAL/ClientesDAL.cs
@@ -4,10 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Lojinha.Modelos;
+using System.Data.SqlClient;
 
 namespace Lojinha.DAL
 {
     public class ClientesDAL
     {
+        SqlConnection cn = new SqlConnection();
+        try 
+	{	        
+		
+	}
+	catch (global::System.Exception)
+	{
+
+		throw;
+	}
     }
 }
