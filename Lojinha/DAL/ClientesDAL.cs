c248c7027be3e0147e3947cdd639f48c2c0d4ddb
Jose <roniele.bio@gmail.com>
2026-05-12 16:07:32 -0400

    Finalizado Clientes.DAL

--- Lojinha/DAL/ClientesDAL.cs
+++ Lojinha/DAL/ClientesDAL.cs
@@ -63,6 +63,145 @@ namespace Lojinha.DAL
                 cn.Close();
             }
 
+
+        }
+        public void Altera(ClienteInformation cliente)
+        {
+            // Conexão com o banco de dados
+            SqlConnection cn = new SqlConnection(Dados.StringConexao);
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "altera_cliente";
+                // Parâmetros da Stored Procedure
+
+
+                // Código
+                SqlParameter pcodigo = new SqlParameter("@codigo", SqlDbType.Int);
+                pcodigo.Direction = ParameterDirection.Output;
+                cmd.Parameters.Add(pcodigo);
+                // Nome
+                SqlParameter pnome = new SqlParameter("@nome", SqlDbType.VarChar, 100);
+                pnome.Value = cliente.Nome;
+                cmd.Parameters.Add(pnome);
+                // Email
+                SqlParameter pemail = new SqlParameter("@email", SqlDbType.VarChar, 100);
+                pemail.Value = cliente.Email;
+                cmd.Parameters.Add(pemail);
+                // Telefone
+                SqlParameter ptelefone = new SqlParameter("@telefone", SqlDbType.VarChar, 20);
+                ptelefone.Value = cliente.Telefone;
+                cmd.Parameters.Add(ptelefone);
+
+                cn.Open();
+                cmd.ExecuteNonQuery();
+
+                cliente.Codigo = (Int32)cmd.Parameters["@codigo"].Value;
+            }
+
+
+            catch (SqlException ex)
+            {
+
+                throw new Exception("Erro ao acessar o banco de dados. " + ex.Number);
+            }
+            catch
+            {
+                throw new Exception("Erro desconhecido ao acessar o banco de dados.");
+
+            }
+            finally
+            {
+                cn.Close();
+            }
+
+
+        }
+        public void Exclui(ClienteInformation cliente)
+        {
+            // Conexão com o banco de dados
+            SqlConnection cn = new SqlConnection(Dados.StringConexao);
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "exclui_cliente";
+                // Parâmetros da Stored Procedure
+
+
+                // Código
+                SqlParameter pcodigo = new SqlParameter("@codigo", SqlDbType.Int);
+                pcodigo.Direction = ParameterDirection.Output;
+                cmd.Parameters.Add(pcodigo);
+
+                cn.Open();
+                cmd.ExecuteNonQuery();
+
+                cliente.Codigo = (Int32)cmd.Parameters["@codigo"].Value;
+            }
+
+
+            catch (SqlException ex)
+            {
+
+                throw new Exception("Erro ao acessar o banco de dados. " + ex.Number);
+            }
+            catch
+            {
+                throw new Exception("Erro desconhecido ao acessar o banco de dados.");
+
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+        public DataTable Listagem(string filtro)
+        {
+            // Conexão com o banco de dados
+            SqlConnection cn = new SqlConnection(Dados.StringConexao);
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "seleciona_cliente";
+                // Parâmetros da Stored Procedure
+
+
+                // Código
+                SqlParameter pfiltro = new SqlParameter("@filtro", SqlDbType.VarChar, 100);
+                pfiltro.Value = filtro;
+                cmd.Parameters.Add(pfiltro);
+                DataTable tabela = new DataTable();
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(tabela);
+                return tabela;
+
+               // cn.Open();
+               // cmd.ExecuteNonQuery();
+
+                //cliente.Codigo = (Int32)cmd.Parameters["@codigo"].Value;
+            }
+
+
+            catch (SqlException ex)
+            {
+
+                throw new Exception("Erro ao acessar o banco de dados. " + ex.Number);
+            }
+            catch
+            {
+                throw new Exception("Erro desconhecido ao acessar o banco de dados.");
+
+            }
+            finally
+            {
+                cn.Close();
+            }
         }
     }
 }
